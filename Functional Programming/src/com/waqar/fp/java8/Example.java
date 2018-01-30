9f62c031a37a1a85039eaec34c15dc85a81d97ca
SHAIKHWA <SHAIKHWA@MUMWD101061.ASIAPAC.NOM>
2018-01-30 16:52:53 +0530

    Functional Programming Implemented

---
+++ Functional Programming/src/com/waqar/fp/java8/Example.java
@@ -0,0 +1,35 @@
+package com.waqar.fp.java8;
+
+import com.waqar.functionalprogramming.FixedCosts;
+import com.waqar.functionalprogramming.IncrementalCosts;
+import com.waqar.functionalprogramming.Sales;
+
+public class Example {
+
+	final static double [] EXPECTED_SALES_JAN_TO_DEC = 
+			new double[] {42.0, 45.6, 43.6, 50.2, 55.6, 54.7, 58.0, 57.3, 62.0, 60.3, 71.2, 88.8};
+	
+	public static void main(String[] args) {
+		
+		final FunctionOverTime sales = 
+				(time) -> EXPECTED_SALES_JAN_TO_DEC[time-1];
+		
+		final FunctionOverTime fixedCosts = 
+				(time) -> 0.15;
+		final FunctionOverTime incrementalCosts = 
+				(time) -> 5.1 + 0.15 * time;
+		
+		
+		final FunctionOverTime profit = 
+				(time) -> sales.valueAt(time) - 
+				(fixedCosts.valueAt(time) +
+						incrementalCosts.valueAt(time));
+		
+		double total = 0.0;
+		for(int i=0; i<=12; i++){
+			total += profit.valueAt(i);
+		}
+		
+		System.out.println("Total profits for the year: "+ total);
+	}
+}
---
+++ Functional Programming/src/com/waqar/fp/java8/FunctionOverTime.java
@@ -0,0 +1,7 @@
+package com.waqar.fp.java8;
+
+@FunctionalInterface
+public interface FunctionOverTime {
+
+	double valueAt(int time);
+}
